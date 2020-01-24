1bf1fb1ecc4f796195e14f79f6da54a866a16c41
Michael Angerman <stormasm@gmail.com>
2020-01-24 07:45:30 -0800

    cc redis_to_file

--- examples/redis_to_file.rs
+++ examples/redis_to_file.rs
@@ -1,16 +1,6 @@
 // Note on vector sorting...
 // https://rust-lang-nursery.github.io/rust-cookbook/algorithms/sorting.html
 
-/*
-Remove this comment once I start adding new stuff to redis...
-File sizes should be
-
-without ids on a separate line
--rw-r--r--  1 ma  wheel  130582 Dec  5 22:00 lines-old.txt
-current implementation
--rw-r--r--  1 ma  wheel  135154 Dec  6 07:42 lines.txt
-*/
-
 use r2d2_redis::{r2d2, RedisConnectionManager};
 use redis::{Commands, RedisResult};
 
@@ -27,7 +17,9 @@ fn get_hashmap_keys(key: String) -> RedisResult<Vec<u32>> {
 }
 
 fn main() -> Result<(), Error> {
-    let mut keys = get_hashmap_keys("hn-story-19".to_string()).unwrap();
+    let hn_story_key = "hn-story-20";
+
+    let mut keys = get_hashmap_keys(hn_story_key.to_string()).unwrap();
     keys.sort();
 
     let manager = RedisConnectionManager::new("redis://localhost").unwrap();
@@ -36,11 +28,11 @@ fn main() -> Result<(), Error> {
     let pool = pool.clone();
     let mut con = pool.get().unwrap();
 
-    let path = "lines.txt";
+    let path = "look.txt";
     let mut output = File::create(path)?;
 
     for key in &keys {
-        let value: RedisResult<String> = con.hget("hn-story-19".to_string(), key.to_string());
+        let value: RedisResult<String> = con.hget(hn_story_key.to_string(), key.to_string());
         let json = value.unwrap();
 
         // println!("{}", key);
